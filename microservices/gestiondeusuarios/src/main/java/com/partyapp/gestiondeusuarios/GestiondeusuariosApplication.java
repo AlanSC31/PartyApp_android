de3ef4769995f6d72bb44bf021b7ae1040673b63
Aln <alan.sanchez3201@alumnos.udg.mx>
2025-04-01 20:11:38 -0600

    se agrego el microservice gestion de usuarios para empezar a trabajar en el

---
+++ microservices/gestiondeusuarios/src/main/java/com/partyapp/gestiondeusuarios/GestiondeusuariosApplication.java
@@ -0,0 +1,13 @@
+package com.partyapp.gestiondeusuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestiondeusuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestiondeusuariosApplication.class, args);
+	}
+
+}
