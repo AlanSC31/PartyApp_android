5c5932b8425db083ddd422e85f487a70e3605126
Aln <alan.sanchez3201@alumnos.udg.mx>
2025-04-03 20:50:10 -0600

    se agrego el titulo e importo la fuente requerida

--- party_app/lib/screens/usuario_register_screen.dart
+++ party_app/lib/screens/usuario_register_screen.dart
@@ -1,6 +1,7 @@
-
 import 'package:flutter/material.dart';
 import 'package:party_app/widgets/gradient_background.dart';
+
+import 'package:google_fonts/google_fonts.dart';
 
 class UsuarioRegister extends StatefulWidget {
   const UsuarioRegister({super.key});
@@ -12,9 +13,36 @@ class UsuarioRegister extends StatefulWidget {
 class _UsuarioRegisterState extends State<UsuarioRegister> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: GradientBackground(child: Center()),
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      body: GradientBackground(
+          child: Center(
+        child: Column(
+          children: [
+            Container(
+              height: screenHeight * 0.40,
+              child: Column(
+                children: [
+                  Center(
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 100),
+                      child: Text(
+                        'PartyApp',
+                        style: GoogleFonts.kronaOne(color: Colors.white,
+                        fontSize: 50),
+                      ),
+                    ),
+                  )
+                  
+                ],
+              ),
+            ),
+            Container(height: screenHeight * 0.60),
+          ],
+        ),
+      )),
     );
-     
   }
 }
