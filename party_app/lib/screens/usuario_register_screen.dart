18cd87cd9ec3c9c5ee9793660755d98a9b6dc0ba
Aln <alan.sanchez3201@alumnos.udg.mx>
2025-04-03 22:26:16 -0600

    usuario register screen terminada

--- party_app/lib/screens/usuario_register_screen.dart
+++ party_app/lib/screens/usuario_register_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:party_app/widgets/gradient_background.dart';
-
 import 'package:google_fonts/google_fonts.dart';
 
 class UsuarioRegister extends StatefulWidget {
@@ -11,38 +10,181 @@ class UsuarioRegister extends StatefulWidget {
 }
 
 class _UsuarioRegisterState extends State<UsuarioRegister> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final nameController = TextEditingController();
+  final lastNameController = TextEditingController();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      body: GradientBackground(
-          child: Center(
+      resizeToAvoidBottomInset: true,  // Para ajustar el contenido al mostrar el teclado
+      body: SafeArea(  // Asegurarse de que el contenido no se sobreponga al área de seguridad de la pantalla
         child: Column(
           children: [
-            Container(
-              height: screenHeight * 0.40,
-              child: Column(
-                children: [
-                  Center(
-                    child: Padding(
-                      padding: const EdgeInsets.only(top: 100),
-                      child: Text(
-                        'PartyApp',
-                        style: GoogleFonts.kronaOne(color: Colors.white,
-                        fontSize: 50),
+            Expanded(  // Esto asegura que el GradientBackground ocupe todo el espacio disponible
+              child: GradientBackground(  // El GradientBackground ocupa toda la pantalla
+                child: SingleChildScrollView(  // Permite desplazarse si el contenido es más grande que la pantalla
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: screenHeight * 0.40,
+                        child: Padding(
+                          padding: const EdgeInsets.only(top: 100),
+                          child: Image.asset(
+                            'assets/logo_title.png',
+                            height: 220,
+                            width: 300,
+                          ),
+                        ),
                       ),
-                    ),
-                  )
-                  
-                ],
+                      // Contenedor con transparencia y bordes redondeados
+                      Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 40),
+                        padding: const EdgeInsets.all(20),
+                        decoration: BoxDecoration(
+                          color: Colors.white.withOpacity(0.1),  // Fondo transparente
+                          borderRadius: BorderRadius.circular(15),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.2),
+                              spreadRadius: 2,
+                              blurRadius: 5,
+                              offset: const Offset(0, 3),
+                            ),
+                          ],
+                        ),
+                        child: Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(top: 20, left: 40, right: 40),
+                              child: TextField(
+                                controller: emailController,
+                                cursorColor: Colors.white,
+                                style: GoogleFonts.biryani(color: Colors.white),
+                                decoration: InputDecoration(
+                                  hintText: 'correo',
+                                  hintStyle: GoogleFonts.biryani(
+                                      color: Colors.white, fontSize: 20),
+                                  enabledBorder: const UnderlineInputBorder(
+                                    borderSide: BorderSide(
+                                        color: Color.fromARGB(255, 206, 205, 205)),
+                                  ),
+                                  focusedBorder: const UnderlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.white),
+                                  ),
+                                  prefixIcon: const Icon(
+                                      Icons.alternate_email_outlined,
+                                      color: Colors.white),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 20, left: 40, right: 40),
+                              child: TextField(
+                                controller: passwordController,
+                                cursorColor: Colors.white,
+                                style: GoogleFonts.biryani(color: Colors.white),
+                                decoration: InputDecoration(
+                                  hintText: 'contraseña',
+                                  hintStyle: GoogleFonts.biryani(
+                                      color: Colors.white, fontSize: 20),
+                                  enabledBorder: const UnderlineInputBorder(
+                                    borderSide: BorderSide(
+                                        color: Color.fromARGB(255, 206, 205, 205)),
+                                  ),
+                                  focusedBorder: const UnderlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.white),
+                                  ),
+                                  prefixIcon: const Icon(Icons.password_sharp,
+                                      color: Colors.white),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 20, left: 40, right: 40),
+                              child: TextField(
+                                controller: nameController,
+                                cursorColor: Colors.white,
+                                style: GoogleFonts.biryani(color: Colors.white),
+                                decoration: InputDecoration(
+                                  hintText: 'nombre(s)',
+                                  hintStyle: GoogleFonts.biryani(
+                                      color: Colors.white, fontSize: 20),
+                                  enabledBorder: const UnderlineInputBorder(
+                                    borderSide: BorderSide(
+                                        color: Color.fromARGB(255, 206, 205, 205)),
+                                  ),
+                                  focusedBorder: const UnderlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.white),
+                                  ),
+                                  prefixIcon: const Icon(Icons.person_2_rounded,
+                                      color: Colors.white),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 20, left: 40, right: 40),
+                              child: TextField(
+                                controller: lastNameController,
+                                cursorColor: Colors.white,
+                                style: GoogleFonts.biryani(color: Colors.white),
+                                decoration: InputDecoration(
+                                  hintText: 'apellido(s)',
+                                  hintStyle: GoogleFonts.biryani(
+                                      color: Colors.white, fontSize: 20),
+                                  enabledBorder: const UnderlineInputBorder(
+                                    borderSide: BorderSide(
+                                        color: Color.fromARGB(255, 206, 205, 205)),
+                                  ),
+                                  focusedBorder: const UnderlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.white),
+                                  ),
+                                  prefixIcon: const Icon(Icons.person_2_rounded,
+                                      color: Colors.white),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 40.0),
+                              child: ElevatedButton(
+                                onPressed: () {},
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor:
+                                      const Color.fromARGB(255, 254, 255, 255)
+                                          .withOpacity(0.2),
+                                  fixedSize: Size(screenWidth * 0.7, 50),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                ),
+                                child: Text(
+                                  'enviar',
+                                  style: GoogleFonts.biryani(
+                                      color: Colors.white, fontSize: 20),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
-            Container(height: screenHeight * 0.60),
           ],
         ),
-      )),
+      ),
     );
   }
 }
